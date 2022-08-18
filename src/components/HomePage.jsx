1e64af4256ef1b6e34478547e0dd07073ac39590
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-18 08:35:40 +0530

    cryptocurrencies

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,14 @@
 import { Typography, Row, Col, Statistic } from "antd";
 import millify from "millify";
 import React from "react";
+import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import CryptoCurrencies from "./CryptoCurrencies";
+import News from "./News";
 
 const { Title } = Typography;
 const HomePage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
   console.log(globalStats);
   if (isFetching) return "Loading....";
@@ -43,6 +46,24 @@ const HomePage = () => {
           />{" "}
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 CryptoCurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies"> Show More</Link>
+        </Title>
+      </div>
+      <CryptoCurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news"> Show More</Link>
+        </Title>
+      </div>
+      <News />
     </>
   );
 };
