7e2977046833f1b0164bdcfda34b27cd935604db
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-18 17:20:50 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Switch, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Layout, Space, Typography } from "antd";
 import {
   Navbar,
