e35584b7de86224d8b2f9379c7beef459aacf9bd
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-18 08:36:17 +0530

    alt added

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -36,7 +36,13 @@ const CryptoCurrencies = ({ simplified }) => {
             <Link to={`/crypto/${currency.id}`} />
             <Card
               title={`${currency.rank}.${currency.name}`}
-              extra={<img className="crypto-image" src={currency.iconUrl} />}
+              extra={
+                <img
+                  className="crypto-image"
+                  src={currency.iconUrl}
+                  alt="crypto"
+                />
+              }
               hoverable
             >
               <p>Price:{millify(currency.price)}</p>
